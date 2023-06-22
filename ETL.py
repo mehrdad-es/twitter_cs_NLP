2855182c7d59bd39228c4395597c23128a57afe0
Mehrdad Esmaeili <public.code.2023@gmail.com>
2023-06-21 23:30:04 -0400

    1

--- ETL.py
+++ ETL.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import datetime as dt
 import calendar
+import string
+import contractions
+import unicodedata
+
+punct_to_remove=string.punctuation
 
 dt_lake = pd.read_csv('./bucket/old_tweets_database.csv')
 # print(dt_lake.loc[1],'\n\n',dt.datetime.strptime(dt_lake.loc[1].date,'%b %d, %Y').weekday())
@@ -10,7 +15,8 @@ txt=[]
 for i in range(dt_lake.shape[0]):
     weekend_vs_weekend.append('w_day' if dt.datetime.strptime(dt_lake.loc[i].date,'%b %d, %Y').weekday()<5 else 'w_end')
     date.append(dt.datetime.strptime(dt_lake.loc[i].date,'%b %d, %Y').strftime('%d/%m/%Y'))
-    txt.append(dt_lake.loc[i].text.replace(',','').replace('\n',' '))
+    txt.append(unicodedata.normalize('NFKD',contractions.fix(dt_lake.loc[i].replace('\n',' ').lower().\
+               translate(str.maketrans('','',punct_to_remove)))).encode('ascii','ignore').decode('utf-8','ignore'))
 dt_lake['Weekday_vs_Weekend']=weekend_vs_weekend
 dt_lake= dt_lake.set_index('month')
 dt_lake.drop(columns=['date','text'],inplace=True)
