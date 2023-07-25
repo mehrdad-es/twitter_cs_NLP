1689990f90e2e2398f18e1ec43247a00181d75e6
Mehrdad Esmaeili <public.code.2023@gmail.com>
2023-07-25 09:20:05 -0400

    1

--- general_tools.py
+++ general_tools.py
@@ -1,5 +1,9 @@
 import datetime as dt
 
+import string
+import contractions
+import unicodedata
+
 def analyze_date_string(date_string):
     '''
     Analyze date string and return a dictionary with date vaules
@@ -8,5 +12,8 @@ def analyze_date_string(date_string):
     datetime_object= dt.datetime.strptime(date_string,'%m/%d/%Y')
     return datetime_object
 
-
+def make_txt_NLP_friendly(text):
+    punct_to_remove=string.punctuation
+    unicodedata.normalize('NFKD',contractions.fix(text.replace('\n',' ').lower().\
+               translate(str.maketrans('','',punct_to_remove)))).encode('ascii','ignore').decode('utf-8','ignore')
 
