d22adf8774974ecfb5def0a0c60dd8f2513ff9f7
Mehrdad Esmaeili <public.code.2023@gmail.com>
2023-07-25 19:52:45 -0400

    1

--- iphone6.sh
+++ iphone6.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
-n=$(/usr/bin/python3 update_cron_parameter.py)
+for ((i=1; i<=36; i++))
+do
+n=$(./venv/bin/python3 update_cron_parameter.py)
 echo $n
-if [ $n -ge 1 -a $n -le 48 ]
+if [ $n -ge 1 -a $n -le 36 ]
 then
-`/usr/bin/python3 sample_search.py $n`
+`./venv/bin/python3 sample_search.py $n`
 fi
+done
+`aws s3 sync s3://tw-storage-2023/train-etl-iphone ./bucket --quiet`
